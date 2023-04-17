70f4c5f20e7ae772f48a664ab699fc809e7671fa
Saisho <santib.2001@hotmail.com>
2023-04-17 18:00:30 -0500

    feat: updated RestCountries App

--- restcountries/src/components/AboutCountry.js
+++ restcountries/src/components/AboutCountry.js
@@ -1,21 +1,21 @@
 import CapitalWeather from "./CapitalWeather"
 
-const AboutCountry = ({country, ApiKey}) => {
-    return (
-        <div>
-        <h1>{country.name.common}</h1>
-        <h3>capital: {country.capital}</h3>
-        <h3>area: {country.area}</h3>
-        <div>
+const AboutCountry = ({ country, ApiKey }) => {
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <h3>capital: {country.capital}</h3>
+      <h3>area: {country.area}</h3>
+      <div>
         <h2>Languages</h2>
         <ul>
-        {Object.values(country.languages).map((language) => (<li key={language}>{language}</li>))}
+          {Object.values(country.languages).map((language) => (<li key={language}>{language}</li>))}
         </ul>
-        </div>
-        <img src={country.flags.png} alt='' width='200'></img>
-        <CapitalWeather capital={country.capital} ApiKey={ApiKey} />
       </div>
-    )
+      <img src={country.flags.png} alt='' width='200'></img>
+      <CapitalWeather capital={country.capital} ApiKey={ApiKey} />
+    </div>
+  )
 }
 
 export default AboutCountry
--- restcountries/src/components/CapitalWeather.js
+++ restcountries/src/components/CapitalWeather.js
@@ -1,27 +1,39 @@
 import { useState, useEffect } from "react"
 import axios from 'axios'
 
-const CapitalWeather = ({capital, ApiKey}) => {
+const CapitalWeather = ({ capital, ApiKey }) => {
 
   const [weather, setWeather] = useState([])
 
-    useEffect(() => {
-      axios
-        .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${ApiKey}&units=Metric`)
-        .then(response => {
-          setWeather(response.data)
-        })
-        console.log('Axiooooos is running')
-    }, [])
+  const params = {
+    access_key: ApiKey,
+    query: 'Chicago'
+  }
 
-    if (weather.length===0){
-      return<p>Cargando...</p>
-    }
-    return(
-      <div>
-        <h3>weather in {capital}</h3>
-        <p>Temperature: {weather.main.temp} Celsius</p>
-      </div>
-    )
+  useEffect(() => {
+    axios.get('http://api.weatherstack.com/current', { params } )
+      .then(response => {
+        setWeather(response.data)
+        console.log('Dentro del axios: ', response.data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [])
+
+  console.log(weather)
+
+  if (weather.length === 0) {
+    return <p>Cargando...</p>
+  }
+
+  return (
+    <div>
+      <h3>weather in {capital}</h3>
+      {/* <p>Temperature: {weather.current.temperature} Celsius</p>
+      <img src={weather.current.weather_icons} alt='100' width='100'></img>
+      <p>Wind: {weather.current.wind_speed} km/h - direction to: {weather.current.wind_dir} </p> */}
+    </div>
+  )
 }
 export default CapitalWeather
